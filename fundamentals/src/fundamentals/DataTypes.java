43ee8529971a3d55ecc9b763cea66079e7a5f714
Dell <Dell@Anu>
2021-07-15 14:50:18 +0530

    module1

---
+++ fundamentals/src/fundamentals/DataTypes.java
@@ -0,0 +1,13 @@
+package fundamentals;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("hello world");
+      System.out.println("hello world");
+      
+ 
+	}
+
+}
